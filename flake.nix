3184e6948dda36c1816f986afcc50f90d35f861b
SlimyZockt <slimyzockt77@gmail.com>
2025-08-21 14:26:41 +0200

    fix wgpu

--- flake.nix
+++ flake.nix
@@ -12,33 +12,45 @@
       pkgs = import nixpkgs {
         inherit system;
       };
-      pakages = with pkgs; [
-        odin
-        raylib
-        # glfw
-        # xorg.libX11
-        # xorg.libXrandr
-        # xorg.libXinerama
-        # xorg.libXcursor
-        # xorg.libXi
-        # xorg.xeyes
-        # mesa
-        # libglvnd
-        wgpu-native
-        wgpu-utils
-      ];
+      wgpu-native-static = pkgs.wgpu-native.overrideAttrs (old: {
+        postInstall = ''
+          install -Dm644 ./ffi/wgpu.h -t $dev/include/webgpu
+          install -Dm644 ./ffi/webgpu-headers/webgpu.h -t $dev/include/webgpu
+        '';
+      });
+
+      odin-with-wgpu = pkgs.odin.overrideAttrs {
+        buildInputs = [ wgpu-native-static ];
+
+        postInstall = ''
+          mkdir $out/share/vendor/wgpu/lib/wgpu-linux-x86_64-release/
+          ln -s ${wgpu-native-static}/lib/ $out/share/vendor/wgpu/lib/wgpu-linux-x86_64-release/
+        '';
+      };
+
+      packages =
+        with pkgs;
+        [
+          raylib
+          wgpu-utils
+          fish
+        ]
+        ++ [
+          odin-with-wgpu
+        ];
     in
     {
       devShells."${system}".default = pkgs.mkShell {
-        nativeBuildInputs = pakages;
+        nativeBuildInputs = packages;
 
         shellHook = ''
-          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath pakages}:$LD_LIBRARY_PATH
+          export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath packages}:$LD_LIBRARY_PATH
           export LIBGL_ALWAYS_SOFTWARE=1
           export DISPLAY=:0
           export XDG_SESSION_TYPE=x11
           export GDK_BACKEND=wayland
           export SDL_VIDEODRIVER=wayland
+
           echo "Odin environment running"
           fish
         '';
